609a89c858c0363bc27ccf0d1ec89c140c242276
dmlsthe1 <dxangobiz@gmail.com>
2017-12-06 17:11:27 -0800

    bonus semi working with if statements

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default class App extends React.Component {
   constructor(){
     super();
@@ -17,6 +18,7 @@ export default class App extends React.Component {
   componentDidMount(){
     this.refs.balance.focus();
   }
+
   handleChange(e) {
     let name = e.target.name;
     let value = e.target.value;
@@ -24,9 +26,11 @@ export default class App extends React.Component {
       [name] : value
     })
   }
+
   handleFocus(e) {
     e.target.select();
   }
+
   handleKeyPress(e) {
     if (e.key == "Enter") {
       if (e.target.name == "balance") {
@@ -40,72 +44,96 @@ export default class App extends React.Component {
       }
     }
   }
-  amortization(principalTotal, rate, numberMonths, monthlyPayments) {
+
+  amortization(totalPrincipal, rate, numberMonths, monthlyPayment) {
     
   }
+
   calculate() {
-    let principalTotal = this.state.balance;
+    let totalPrincipal = this.state.balance;
     let rate = this.state.rate / 1200;
     let numberMonths = this.state.term * 12;
-    let monthlyPayments = (principalTotal * ((rate * Math.pow((1 + rate), numberMonths)) / (Math.pow((1 + rate), numberMonths) - 1)));
-    this.amortization(principalTotal, rate, numberMonths, monthlyPayments);
+    let monthlyPayment = (totalPrincipal * ((rate * Math.pow((1 + rate), numberMonths)) / (Math.pow((1 + rate), numberMonths) - 1))).toFixed(2);
     let obj = {
-      Amount: parseFloat(monthlyPayments),
-      Interest: parseFloat((rate * principalTotal)),
-      Principal: parseFloat(((monthlyPayments - (rate* principalTotal)))),
-      Balance: parseFloat(principalTotal - (monthlyPayments - (rate * principalTotal)))
+      Amount: parseFloat(monthlyPayment),
+      Interest: parseFloat((rate * totalPrincipal)),
+      Principal: parseFloat(((monthlyPayment - (rate * totalPrincipal)))),
+      Balance: parseFloat(totalPrincipal - (monthlyPayment - (rate * totalPrincipal)))
     };
     let tableContents = [];
     for (let i = 0; i < numberMonths ; i++){
       obj.Payment = (i + 1);
-      
-      (i + 1) == numberMonths ? (obj.Payment = "Final Payment", obj.Amount = ( obj.nextInterest + obj.nextBalance)) : "";
+      if (i + 1 === 1) {
+        tableContents.push(<tr key={i}>
+          <td className="text-center">{obj.Payment}</td>
+          <td className="text-center">{obj.Amount.toFixed(2)}</td>
+          <td className="text-center">{obj.Interest.toFixed(2)}</td>
+          <td className="text-center">{obj.Principal.toFixed(2)}</td>
+          <td className="text-center">{obj.Balance.toFixed(2)}</td>
+        </tr>);
+      //(i + 1) == numberMonths ? (obj.Payment = "Final Payment", console.log(obj.Interest), obj.Amount = obj.nextBalance, obj.Balance = obj.nextBalance - obj.nextInterest, console.log(obj.nextInterest)) : "";
       //obj.Amount = (obj.Balance <= obj.Amount) ? (obj.Balance + (rate * obj.Balance)) : obj.Amount;
-      
-      tableContents.push(<tr key={i}>
-        <td className="text-center">{obj.Payment}</td>
-        <td className="text-center">{obj.Amount.toFixed(2)}</td>
-        <td className="text-center">{obj.Interest.toFixed(2)}</td>
-        <td className="text-center">{obj.Principal.toFixed(2)}</td>
-        <td className="text-center">{obj.Balance.toFixed(2)}</td>
-      </tr>);
-      
-      obj.Interest =       (rate * obj.Balance);
-      obj.Principal =      (obj.Amount - obj.Interest);
-      obj.Balance =        (obj.Balance - obj.Principal);
-      obj.nextInterest =   (rate * obj.Balance);
-      obj.nextPrincipal =  (obj.Amount - obj.nextInterest);
-      obj.nextBalance =    (obj.Balance + obj.nextPrincipal);
+      } else if (i + 1 === numberMonths) {
+        console.log(obj.Principal, obj.Interest, obj.Balance); 
+        obj.Interest = rate * obj.Balance;
+        obj.Amount = obj.Balance + obj.Interest;
+        obj.Principal = obj.Amount - obj.Interest;
+        obj.Balance = obj.Balance - obj.Principal;
+        console.log(obj.Principal, obj.Interest, obj.Balance);
+        tableContents.push(<tr key={i}>
+          <td className="text-center">Final Payment</td>
+          <td className="text-center">{obj.Amount.toFixed(2)}</td>
+          <td className="text-center">{obj.Interest.toFixed(2)}</td>
+          <td className="text-center">{obj.Principal.toFixed(2)}</td>
+          <td className="text-center">{obj.Balance.toFixed(2)}</td>
+        </tr>);
+      } else {
+        obj.Interest = (rate * obj.Balance);
+        obj.Principal = (obj.Amount - obj.Interest);
+        obj.Balance = (obj.Balance - obj.Principal);
+        tableContents.push(<tr key={i}>
+          <td className="text-center">{obj.Payment}</td>
+          <td className="text-center">{obj.Amount.toFixed(2)}</td>
+          <td className="text-center">{obj.Interest.toFixed(2)}</td>
+          <td className="text-center">{obj.Principal.toFixed(2)}</td>
+          <td className="text-center">{obj.Balance.toFixed(2)}</td>
+        </tr>);
+      }
     }
     this.setState({
       clicked: true,
-      output: monthlyPayments.toFixed(2),
+      output: monthlyPayment,
       amortization: tableContents,
     });
     this.refs.balance.focus();
   }
+
   render() {
     return (
         
         <div className='container pt-5'>
           
             <form>
+
               <div className="shadow row mb-3">
                 <div className="col-3"></div>
                 <h1 className="title col-9">Mortgage Calculator</h1>
               </div>
+
               <div className="shadow form-group row">
                 <label htmlFor="idBalance" className="col-md-3 text-center text-md-right my-2"><b>Loan Balance</b></label>
                 <div className="col-md-9">
                   <input id="idBalance" type="number" onChange={this.handleChange} onFocus={this.handleFocus} onKeyPress={this.handleKeyPress} ref="balance" placeholder="Principal" name="balance" className="form-control"></input>
                 </div>
               </div>
+
               <div className="shadow form-group row">
                 <label htmlFor="idInterest" className="col-md-3 text-center text-md-right my-2"><b>Interest Rate (%)</b></label>
                 <div className="col-md-9">
                   <input id="idInterest" type="number" onChange={this.handleChange} onFocus={this.handleFocus} onKeyPress={this.handleKeyPress} ref="rate" placeholder="APR%" name="rate" step="0.01" className="form-control"></input>
                 </div>
               </div>
+
               <div className="shadow row">
                 <label htmlFor="idTerm" className="col-md-3 text-center text-md-right my-2"><b>Loan Term (years)</b></label>
                 <div className="col-md-9">
@@ -116,12 +144,14 @@ export default class App extends React.Component {
                   </select>
                 </div>
               </div>
+
               <div className="row my-3">
                 <div className="col-3"></div>
                 <div className="col">
                   <button className="btn btn-primary form-control" onClick={this.calculate} name="submit" type="button">{this.state.clicked ? "Calculate Again" : "Calculate"}</button>
                 </div>
               </div>
+
               <div id="output" className="mt-5">
                 {
                 this.state.output ?
@@ -145,6 +175,7 @@ export default class App extends React.Component {
                 ""
                 }
               </div>
+
             </form>
           
         </div>
